c437102a8c9f997d58ad8ed6332427ffc13e3d53
Villex <videlik@gmail.com>
2023-02-14 20:59:03 +0200

    i love god

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,5 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+// pages/api/auth/[...auth0].js
+import auth0 from "../../../utils/auth0";
 
-export default handleAuth();
+// Use the instance method
+export default auth0.handleAuth();
